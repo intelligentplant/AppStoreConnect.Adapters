d1ce94b1c92f85ba91321436efcc52de1928f6c3
Graham Watts <grahamcharleswatts@gmail.com>
2019-03-25 13:19:32 +0200

    Create AdapterDescriptorExtendedExtensions.cs

---
+++ src/DataCore.Adapter/Common/Models/AdapterDescriptorExtendedExtensions.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataCore.Adapter.Common.Models {
+
+    /// <summary>
+    /// Extensions for <see cref="AdapterDescriptorExtended"/>.
+    /// </summary>
+    public static class AdapterDescriptorExtendedExtensions {
+
+        /// <summary>
+        /// Tests if the descriptor contains the specified feature in its <see cref="AdapterDescriptorExtended.Features"/> 
+        /// list.
+        /// </summary>
+        /// <typeparam name="TFeature">
+        ///   The feature type.
+        /// </typeparam>
+        /// <param name="descriptor">
+        ///   The descriptor.
+        /// </param>
+        /// <returns>
+        ///   <see langword="true"/> if the feature is in the list, or <see langword="false"/> 
+        ///   otherwise.
+        /// </returns>
+        public static bool HasFeature<TFeature>(this AdapterDescriptorExtended descriptor) where TFeature : IAdapterFeature {
+            if (descriptor == null) {
+                return false;
+            }
+
+            return descriptor.HasFeature(typeof(TFeature).FullName);
+        }
+
+
+        /// <summary>
+        /// Tests if the descriptor contains the specified feature in its <see cref="AdapterDescriptorExtended.Features"/> 
+        /// list.
+        /// </summary>
+        /// <param name="descriptor">
+        ///   The descriptor.
+        /// </param>
+        /// <param name="featureName">
+        ///   The feature name.
+        /// </param>
+        /// <returns>
+        ///   <see langword="true"/> if the feature is in the list, or <see langword="false"/> 
+        ///   otherwise.
+        /// </returns>
+        public static bool HasFeature(this AdapterDescriptorExtended descriptor, string featureName) {
+            if (descriptor == null) {
+                return false;
+            }
+
+            return descriptor.Features.Any(f => String.Equals(f, featureName));
+        }
+
+    }
+}
