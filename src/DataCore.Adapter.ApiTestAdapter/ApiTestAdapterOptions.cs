c24998a710580df7f94c1728a5b3e98122de9d14
Graham Watts <grahamcharleswatts@gmail.com>
2019-09-05 11:12:38 +0300

    Remove test adapter project

--- src/DataCore.Adapter.ApiTestAdapter/ApiTestAdapterOptions.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace DataCore.Adapter.ApiTestAdapter {
-    public class ApiTestAdapterOptions : Csv.CsvAdapterOptions {
-    }
-}
