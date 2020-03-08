31e54320956f8974ac6e16a0f82e1c3847014bd7
Graham Watts <grahamcharleswatts@gmail.com>
2020-03-08 18:40:02 +0200

    Add options schema classes
    Add classes for describing adapter options schema, for parsing string to strongly-typed adapter options.

---
+++ src/DataCore.Adapter/Configuration/AdapterOptionsSchema.cs
@@ -0,0 +1,24 @@
+﻿namespace DataCore.Adapter.Configuration {
+
+    /// <summary>
+    /// A schema that describes the settings that can be configured for an adapter.
+    /// </summary>
+    public class AdapterOptionsSchema {
+
+        /// <summary>
+        /// Content type for JSON schemas.
+        /// </summary>
+        public const string JsonSchema = "application/schema+json";
+
+        /// <summary>
+        /// The content type of the schema. <see cref="JsonSchema"/> is assumed by default.
+        /// </summary>
+        public string ContentType { get; set; } = JsonSchema;
+
+        /// <summary>
+        /// The schema.
+        /// </summary>
+        public string Schema { get; set; }
+
+    }
+}
---
+++ src/DataCore.Adapter/Configuration/IAdapterOptionsParser.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+
+namespace DataCore.Adapter.Configuration {
+
+    /// <summary>
+    /// Describes a parser that can parse options for an adapter based on a pre-defined text schema.
+    /// </summary>
+    /// <typeparam name="TAdapter">
+    ///   The adapter type.
+    /// </typeparam>
+    /// <typeparam name="TAdapterOptions">
+    ///   The adapter options type.
+    /// </typeparam>
+    public interface IAdapterOptionsParser<TAdapter, TAdapterOptions> 
+        where TAdapter : AdapterBase<TAdapterOptions> 
+        where TAdapterOptions : AdapterOptions, new() 
+    {
+
+        /// <summary>
+        /// Gets the schema for the adapter options.
+        /// </summary>
+        /// <returns>
+        ///   The options schema.
+        /// </returns>
+        AdapterOptionsSchema GetSchema();
+
+
+        /// <summary>
+        /// Tries to parse the provided input string using the schema.
+        /// </summary>
+        /// <param name="s">
+        ///   The string to parse.
+        /// </param>
+        /// <param name="options">
+        ///   The parsed options.
+        /// </param>
+        /// <param name="errors">
+        ///   The validation errors, if the return value is <see langword="false"/>.
+        /// </param>
+        /// <returns>
+        ///   <see langword="true"/> if the string was successfully parsed, or 
+        ///   <see langword="false"/> otherwise.
+        /// </returns>
+        /// <remarks>
+        ///   When returning <see langword="false"/>, implementers should ensure that the 
+        ///   <paramref name="errors"/> parameter is populated.
+        /// </remarks>
+        bool TryParse(string s, out TAdapterOptions options, out IEnumerable<ValidationError> errors);
+
+
+        /// <summary>
+        /// Converts the specified adapter options to a string using the parser's schema.
+        /// </summary>
+        /// <param name="options">
+        ///   The adapter options.
+        /// </param>
+        /// <returns>
+        ///   The serialized adapter options.
+        /// </returns>
+        string ToString(TAdapterOptions options);
+
+    }
+}
