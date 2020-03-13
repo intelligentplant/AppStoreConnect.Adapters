ff9d3b7761ef0a5e7651a83ed9f66271d979e417
Graham Watts <grahamcharleswatts@gmail.com>
2020-03-13 09:56:04 +0200

    Create DefaultAdapterCallContext.cs

---
+++ src/DataCore.Adapter/DefaultAdapterCallContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace DataCore.Adapter {
+
+    /// <summary>
+    /// Default <see cref="IAdapterCallContext"/> implementation.
+    /// </summary>
+    public sealed class DefaultAdapterCallContext : IAdapterCallContext {
+
+        /// <inheritdoc/>
+        public ClaimsPrincipal User { get; }
+
+        /// <inheritdoc/>
+        public string ConnectionId { get; }
+
+        /// <inheritdoc/>
+        public string CorrelationId { get; }
+
+        /// <inheritdoc/>
+        public IDictionary<object, object> Items { get; }
+
+
+        /// <summary>
+        /// Creates a new <see cref="DefaultAdapterCallContext"/> object.
+        /// </summary>
+        /// <param name="user">
+        ///   The user.
+        /// </param>
+        /// <param name="connectionId">
+        ///   The connection ID.
+        /// </param>
+        /// <param name="correlationId">
+        ///   The correlation ID.
+        /// </param>
+        public DefaultAdapterCallContext(
+            ClaimsPrincipal user = null,
+            string connectionId = null,
+            string correlationId = null
+        ) {
+            User = user;
+            ConnectionId = connectionId ?? Guid.NewGuid().ToString();
+            CorrelationId = correlationId ?? Guid.NewGuid().ToString();
+            Items = new ConcurrentDictionary<object, object>();
+        }
+
+    }
+}
