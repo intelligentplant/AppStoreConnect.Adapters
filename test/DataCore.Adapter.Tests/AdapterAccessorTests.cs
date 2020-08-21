2866c3a16b178b5b0c24a34c15a67e6262fb0331
Graham Watts <grahamcharleswatts@gmail.com>
2020-08-21 17:42:24 +0300

    Fix test

--- test/DataCore.Adapter.Tests/AdapterAccessorTests.cs
+++ test/DataCore.Adapter.Tests/AdapterAccessorTests.cs
@@ -155,6 +155,7 @@ namespace DataCore.Adapter.Tests {
         [TestMethod]
         public async Task AdapterAccessor_ShouldResolveAndAuthorizeExtensionFeatureUri() {
             using (var adapter = new ExampleAdapter()) {
+                ((AdapterFeaturesCollection) adapter.Features).Add<ITestExtension, TestExtension>(new TestExtension());
                 var authService = new AuthorizationService(true);
                 var accessor = new AdapterAccessorImpl(adapter, authService);
 
