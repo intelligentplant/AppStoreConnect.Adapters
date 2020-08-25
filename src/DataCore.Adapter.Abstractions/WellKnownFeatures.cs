c3ce2259ed321d15f95297cdde942593ef038250
Graham Watts <grahamcharleswatts@gmail.com>
2020-08-25 09:44:43 +0300

    Update WellKnownFeatures.cs
    Add trailing slash to all standard feature URIs

--- src/DataCore.Adapter.Abstractions/WellKnownFeatures.cs
+++ src/DataCore.Adapter.Abstractions/WellKnownFeatures.cs
@@ -5,26 +5,27 @@ using DataCore.Adapter.RealTimeData;
 
 namespace DataCore.Adapter {
 
+#pragma warning disable CA1034 // Nested types should not be visible
+
     /// <summary>
     /// Defines URIs for well-known adapter features.
     /// </summary>
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1034:Nested types should not be visible", Justification = "Visible nested types required for grouping related constants")]
     public static class WellKnownFeatures {
 
         /// <summary>
         /// Defines URIs for well-known asset model adapter features.
         /// </summary>
-        public static class AssetModel {
 
+        public static class AssetModel {
             /// <summary>
             /// URI for <see cref="IAssetModelBrowse"/>.
             /// </summary>
-            public const string AssetModelBrowse = "asc:features/asset-model/browse";
+            public const string AssetModelBrowse = "asc:features/asset-model/browse/";
 
             /// <summary>
             /// URI for <see cref="IAssetModelSearch"/>.
             /// </summary>
-            public const string AssetModelSearch = "asc:features/asset-model/search";
+            public const string AssetModelSearch = "asc:features/asset-model/search/";
 
         }
 
@@ -37,7 +38,7 @@ namespace DataCore.Adapter {
             /// <summary>
             /// URI for <see cref="IHealthCheck"/>.
             /// </summary>
-            public const string HealthCheck = "asc:features/diagnostics/health-check";
+            public const string HealthCheck = "asc:features/diagnostics/health-check/";
 
         }
 
@@ -50,27 +51,27 @@ namespace DataCore.Adapter {
             /// <summary>
             /// URI for <see cref="IEventMessagePush"/>.
             /// </summary>
-            public const string EventMessagePush = "asc:features/events/push";
+            public const string EventMessagePush = "asc:features/events/push/";
 
             /// <summary>
             /// URI for <see cref="IEventMessagePushWithTopics"/>.
             /// </summary>
-            public const string EventMessagePushWithTopics = "asc:features/events/push/topics";
+            public const string EventMessagePushWithTopics = "asc:features/events/push/topics/";
 
             /// <summary>
             /// URI for <see cref="IReadEventMessagesForTimeRange"/>.
             /// </summary>
-            public const string ReadEventMessagesForTimeRange = "asc:features/events/read/time";
+            public const string ReadEventMessagesForTimeRange = "asc:features/events/read/time/";
 
             /// <summary>
             /// URI for <see cref="IReadEventMessagesUsingCursor"/>.
             /// </summary>
-            public const string ReadEventMessagesUsingCursor = "asc:features/events/read/cursor";
+            public const string ReadEventMessagesUsingCursor = "asc:features/events/read/cursor/";
 
             /// <summary>
             /// URI for <see cref="IWriteEventMessages"/>.
             /// </summary>
-            public const string WriteEventMessages = "asc:features/events/write";
+            public const string WriteEventMessages = "asc:features/events/write/";
 
         }
 
@@ -83,64 +84,67 @@ namespace DataCore.Adapter {
             /// <summary>
             /// URI for <see cref="IReadTagValueAnnotations"/>.
             /// </summary>
-            public const string ReadAnnotations = "asc:features/real-time-data/annotations/read";
+            public const string ReadAnnotations = "asc:features/real-time-data/annotations/read/";
 
             /// <summary>
             /// URI for <see cref="IReadPlotTagValues"/>.
             /// </summary>
-            public const string ReadPlotTagValues = "asc:features/real-time-data/values/read/plot";
+            public const string ReadPlotTagValues = "asc:features/real-time-data/values/read/plot/";
 
             /// <summary>
             /// URI for <see cref="IReadProcessedTagValues"/>.
             /// </summary>
-            public const string ReadProcessedTagValues = "asc:features/real-time-data/values/read/processed";
+            public const string ReadProcessedTagValues = "asc:features/real-time-data/values/read/processed/";
 
             /// <summary>
             /// URI for <see cref="IReadRawTagValues"/>.
             /// </summary>
-            public const string ReadRawTagValues = "asc:features/real-time-data/values/read/raw";
+            public const string ReadRawTagValues = "asc:features/real-time-data/values/read/raw/";
 
             /// <summary>
             /// URI for <see cref="IReadSnapshotTagValues"/>.
             /// </summary>
-            public const string ReadSnapshotTagValues = "asc:features/real-time-data/values/read/snapshot";
+            public const string ReadSnapshotTagValues = "asc:features/real-time-data/values/read/snapshot/";
 
             /// <summary>
             /// URI for <see cref="IReadTagValuesAtTimes"/>.
             /// </summary>
-            public const string ReadTagValuesAtTimes = "asc:features/real-time-data/values/read/at-times";
+            public const string ReadTagValuesAtTimes = "asc:features/real-time-data/values/read/at-times/";
 
             /// <summary>
             /// URI for <see cref="ISnapshotTagValuePush"/>.
             /// </summary>
-            public const string SnapshotTagValuePush = "asc:features/real-time-data/values/push";
+            public const string SnapshotTagValuePush = "asc:features/real-time-data/values/push/";
 
             /// <summary>
             /// URI for <see cref="ITagInfo"/>.
             /// </summary>
-            public const string TagInfo = "asc:features/real-time-data/tags/info";
+            public const string TagInfo = "asc:features/real-time-data/tags/info/";
 
             /// <summary>
             /// URI for <see cref="ITagSearch"/>.
             /// </summary>
-            public const string TagSearch = "asc:features/real-time-data/tags/search";
+            public const string TagSearch = "asc:features/real-time-data/tags/search/";
 
             /// <summary>
             /// URI for <see cref="IWriteTagValueAnnotations"/>.
             /// </summary>
-            public const string WriteAnnotations = "asc:features/real-time-data/annotations/write";
+            public const string WriteAnnotations = "asc:features/real-time-data/annotations/write/";
 
             /// <summary>
             /// URI for <see cref="IWriteHistoricalTagValues"/>.
             /// </summary>
-            public const string WriteHistoricalTagValues = "asc:features/real-time-data/values/write/history";
+            public const string WriteHistoricalTagValues = "asc:features/real-time-data/values/write/history/";
 
             /// <summary>
             /// URI for <see cref="IWriteSnapshotTagValues"/>.
             /// </summary>
-            public const string WriteSnapshotTagValues = "asc:features/real-time-data/values/write/snapshot";
+            public const string WriteSnapshotTagValues = "asc:features/real-time-data/values/write/snapshot/";
 
         }
 
     }
+
+#pragma warning restore CA1034 // Nested types should not be visible
+
 }
