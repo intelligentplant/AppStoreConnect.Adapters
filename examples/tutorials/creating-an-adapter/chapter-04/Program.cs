63b1071070d404a48776b61049435cb3932cc143
Graham Watts <grahamcharleswatts@gmail.com>
2020-09-01 14:57:54 +0300

    Update tutorial

--- examples/tutorials/creating-an-adapter/chapter-04/Program.cs
+++ examples/tutorials/creating-an-adapter/chapter-04/Program.cs
@@ -2,7 +2,6 @@
 using System.Threading;
 using System.Threading.Tasks;
 using DataCore.Adapter;
-using DataCore.Adapter.Diagnostics;
 using DataCore.Adapter.RealTimeData;
 
 namespace MyAdapter {
@@ -29,7 +28,6 @@ namespace MyAdapter {
 
         private static async Task Run(IAdapterCallContext context, CancellationToken cancellationToken) {
             await using (IAdapter adapter = new Adapter(AdapterId, AdapterDisplayName, AdapterDescription)) {
-
                 await adapter.StartAsync(cancellationToken);
 
                 Console.WriteLine();
@@ -48,9 +46,7 @@ namespace MyAdapter {
                 var tagSearchFeature = adapter.GetFeature<ITagSearch>();
                 var snapshotPushFeature = adapter.GetFeature<ISnapshotTagValuePush>();
 
-                using (var subscription = await snapshotPushFeature.Subscribe(context, new CreateSnapshotTagValueSubscriptionRequest()))
-                using (cancellationToken.Register(() => subscription.Cancel())) {
-                    var tags = await tagSearchFeature.FindTags(
+                var tags = await tagSearchFeature.FindTags(
                         context,
                         new FindTagsRequest() {
                             Name = "Sin*",
@@ -59,30 +55,32 @@ namespace MyAdapter {
                         cancellationToken
                     );
 
-                    await tags.WaitToReadAsync(cancellationToken);
-                    tags.TryRead(out var tag);
+                await tags.WaitToReadAsync(cancellationToken);
+                tags.TryRead(out var tag);
 
-                    Console.WriteLine();
-                    Console.WriteLine("[Tag Details]");
-                    Console.WriteLine($"  Name: {tag.Name}");
-                    Console.WriteLine($"  ID: {tag.Id}");
-                    Console.WriteLine($"  Description: {tag.Description}");
-                    Console.WriteLine("  Properties:");
-                    foreach (var prop in tag.Properties) {
-                        Console.WriteLine($"    - {prop.Name} = {prop.Value}");
-                    }
-
-                    await subscription.AddTagToSubscription(tag.Id);
-
-                    Console.WriteLine("  Snapshot Value:");
-                    subscription.Reader.RunBackgroundOperation(async (ch, ct) => {
-                        await foreach (var value in ch.ReadAllAsync(ct)) {
-                            Console.WriteLine($"    - {value.Value}");
-                        }
-                    }, null, cancellationToken);
-
-                    await subscription.Completed;
+                Console.WriteLine();
+                Console.WriteLine("[Tag Details]");
+                Console.WriteLine($"  Name: {tag.Name}");
+                Console.WriteLine($"  ID: {tag.Id}");
+                Console.WriteLine($"  Description: {tag.Description}");
+                Console.WriteLine("  Properties:");
+                foreach (var prop in tag.Properties) {
+                    Console.WriteLine($"    - {prop.Name} = {prop.Value}");
                 }
+
+                var subscription = await snapshotPushFeature.Subscribe(context, new CreateSnapshotTagValueSubscriptionRequest() {
+                    Tags = new[] { tag.Id },
+                    PublishInterval = TimeSpan.FromSeconds(1)
+                }, cancellationToken);
+
+                subscription.RunBackgroundOperation(async (ch, ct) => {
+                    Console.WriteLine("  Snapshot Value:");
+                    await foreach (var value in ch.ReadAllAsync(ct)) {
+                        Console.WriteLine($"    - {value.Value}");
+                    }
+                }, null, cancellationToken);
+
+                await subscription.Completion;
             }
         }
 
