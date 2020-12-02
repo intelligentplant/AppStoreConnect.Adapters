8d090f8df600b100f06a5b21f66edd04ac7b2dec
Graham Watts <grahamcharleswatts@gmail.com>
2020-12-02 13:31:56 +0200

    Update DataFunctionStatusType.cs
    Add additional DataFunctionStatusType values.

--- src/DataCore.Adapter.Core/RealTimeData/DataFunctionStatusType.cs
+++ src/DataCore.Adapter.Core/RealTimeData/DataFunctionStatusType.cs
@@ -8,22 +8,38 @@
         /// <summary>
         /// The computation method is not specified.
         /// </summary>
-        Unspecified,
+        Unspecified = 1,
 
         /// <summary>
         /// The status is calculated based on a percentage of value counts.
         /// </summary>
-        PercentValues,
+        PercentValues = 2,
 
         /// <summary>
         /// The status is calculated based on a percentage of the time interval.
         /// </summary>
-        PercentTime,
+        PercentTime = 3,
 
         /// <summary>
         /// The status calculation is custom to the data function.
         /// </summary>
-        Custom
+        Custom = 4,
+
+        /// <summary>
+        /// The quality status matches the status of a raw value selected by the function.
+        /// </summary>
+        Raw = 5,
+
+        /// <summary>
+        /// The status calculation always returns <see cref="TagValueStatus.Good"/>.
+        /// </summary>
+        AlwaysGood = 6,
+
+        /// <summary>
+        /// The quality status is the worst-case of all of the samples in the sample bucket used 
+        /// to calculate the data function result.
+        /// </summary>
+        WorstCase = 7
 
     }
 }
