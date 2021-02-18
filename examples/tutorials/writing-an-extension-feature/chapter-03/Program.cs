f000b9521e5ea0314f2b5a71bde892ecdc4d23f4
Graham Watts <grahamcharleswatts@gmail.com>
2021-02-18 15:17:02 +0200

    Update tutorial code

--- examples/tutorials/writing-an-extension-feature/chapter-03/Program.cs
+++ examples/tutorials/writing-an-extension-feature/chapter-03/Program.cs
@@ -1,83 +1,22 @@
-﻿using System;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
-using DataCore.Adapter;
-using DataCore.Adapter.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace MyAdapter {
     class Program {
 
-        private const string AdapterId = "example";
-
-        private const string AdapterDisplayName = "Example Adapter";
-
-        private const string AdapterDescription = "Example adapter with an extension feature, built using the tutorial on GitHub";
-
-
-        static async Task Main(string[] args) {
-            using (var userCancelled = new CancellationTokenSource()) {
-                Console.CancelKeyPress += (sender, e) => userCancelled.Cancel();
-                try {
-                    Console.WriteLine("Press CTRL+C to quit");
-                    await Run(new DefaultAdapterCallContext(), userCancelled.Token);
-                }
-                catch (OperationCanceledException) { }
-            }
+        public static async Task Main(params string[] args) {
+            await CreateHostBuilder(args).RunConsoleAsync().ConfigureAwait(false);
         }
 
 
-        private static async Task Run(IAdapterCallContext context, CancellationToken cancellationToken) {
-            using (IAdapter adapter = new Adapter(AdapterId, AdapterDisplayName, AdapterDescription)) {
-                await adapter.StartAsync(cancellationToken);
-
-                var adapterDescriptor = adapter.CreateExtendedAdapterDescriptor();
-
-                Console.WriteLine();
-                Console.WriteLine($"[{adapter.Descriptor.Id}]");
-                Console.WriteLine($"  Name: {adapter.Descriptor.Name}");
-                Console.WriteLine($"  Description: {adapter.Descriptor.Description}");
-                Console.WriteLine("  Properties:");
-                foreach (var prop in adapter.Properties) {
-                    Console.WriteLine($"    - {prop.Name} = {prop.Value}");
-                }
-                Console.WriteLine("  Features:");
-                foreach (var feature in adapterDescriptor.Features) {
-                    Console.WriteLine($"    - {feature}");
-                }
-                Console.WriteLine("  Extensions:");
-                foreach (var feature in adapterDescriptor.Extensions) {
-                    var extension = adapter.GetFeature<IAdapterExtensionFeature>(feature);
-                    var extensionDescriptor = await extension.GetDescriptor(context, feature, cancellationToken);
-                    var extensionOps = await extension.GetOperations(context, feature, cancellationToken);
-                    Console.WriteLine($"    - {feature}");
-                    Console.WriteLine($"      - Name: {extensionDescriptor.DisplayName}");
-                    Console.WriteLine($"      - Description: {extensionDescriptor.Description}");
-                    Console.WriteLine($"      - Operations:");
-                    foreach (var op in extensionOps) {
-                        Console.WriteLine($"        - {op.Name} ({op.OperationId})");
-                        Console.WriteLine($"          - Description: {op.Description}");
-                    }
-                }
-
-                var extensionFeature = adapter.GetFeature<IAdapterExtensionFeature>("asc:extensions/tutorial/ping-pong/");
-                var correlationId = Guid.NewGuid().ToString();
-                var pingMessage = new PingMessage() { CorrelationId = correlationId };
-                var pongMessageStream = await extensionFeature.Stream<PingMessage, PongMessage>(
-                    context,
-                    new Uri("asc:extensions/tutorial/ping-pong/Ping/Stream/"),
-                    pingMessage,
-                    cancellationToken
-                );
-
-                Console.WriteLine();
-                Console.WriteLine($"[STREAM] Ping: {pingMessage.CorrelationId} @ {pingMessage.UtcTime:HH:mm:ss} UTC");
-                await foreach (var pongMessage in pongMessageStream.ReadAllAsync(cancellationToken)) {
-                    Console.WriteLine($"[STREAM] Pong: {pongMessage.CorrelationId} @ {pongMessage.UtcTime:HH:mm:ss} UTC");
-                }
-
-            }
+        private static IHostBuilder CreateHostBuilder(string[] args) {
+            return Host.CreateDefaultBuilder(args).ConfigureServices(services => {
+                services.AddHostedService<Runner>();
+            });
         }
 
     }
+
 }
