1f390e773036e607c27baf88c0d36d8778b72b90
Graham Watts <grahamcharleswatts@gmail.com>
2021-03-09 10:16:43 +0200

    Variants can define extension objects as their value
    - `ExtensionObject` is analogous to the same concept in OPC UA i.e. it can be used to define a custom `Variant` value that is not one of the standard variant types. Extension objects specify a type identifier that can be used by the consumer to identify the type, an encoded body containing the actual object, and an encoding type (such as `application/json`) so that the consumer can correctly decode the object.
    - In addition to encoding `ExtensionObject` instances to/from JSON, `ExtensionObjectConverter` also defines a static `CreateExtensionObject<T>` method that allows JSON-encoded extension objects to be easily created.

---
+++ src/DataCore.Adapter.Core/Common/ExtensionObject.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+
+namespace DataCore.Adapter.Common {
+
+    /// <summary>
+    /// Describes a non-standard <see cref="Variant"/> value.
+    /// </summary>
+    public class ExtensionObject : IEquatable<ExtensionObject> {
+
+        /// <summary>
+        /// The URI that defines the extension object type.
+        /// </summary>
+        public Uri TypeId { get; }
+
+        /// <summary>
+        /// The content type for the <see cref="EncodedBody"/>.
+        /// </summary>
+        public string Encoding { get; }
+
+        /// <summary>
+        /// The encoded object body.
+        /// </summary>
+        public byte[] EncodedBody { get; }
+
+
+        /// <summary>
+        /// Creates a new <see cref="ExtensionObject"/> object.
+        /// </summary>
+        /// <param name="typeId">
+        ///   The URI that defines the extension object type.
+        /// </param>
+        /// <param name="encoding">
+        ///   The content type for the encoded object value.
+        /// </param>
+        /// <param name="encodedBody">
+        ///   The encoded object value.
+        /// </param>
+        public ExtensionObject(Uri typeId, string encoding, byte[] encodedBody) {
+            TypeId = typeId ?? throw new ArgumentNullException(nameof(typeId));
+            Encoding = encoding ?? throw new ArgumentNullException(nameof(encoding));
+            EncodedBody = encodedBody ?? throw new ArgumentNullException(nameof(encodedBody));
+        }
+
+
+        /// <inheritdoc/>
+        public override int GetHashCode() {
+#if NETSTANDARD2_0 || NET46
+            return HashGenerator.Combine(TypeId, Encoding, EncodedBody);
+#else
+            return HashCode.Combine(TypeId, Encoding, EncodedBody);
+#endif
+        }
+
+
+        /// <inheritdoc/>
+        public override bool Equals(object obj) {
+            return obj is ExtensionObject eo 
+                ? Equals(eo) 
+                : false;
+        }
+
+
+        /// <inheritdoc/>
+        public bool Equals(ExtensionObject other) {
+            if (other == null) {
+                return false;
+            }
+
+            return TypeId.Equals(other.TypeId) && 
+                Encoding.Equals(other.Encoding, StringComparison.OrdinalIgnoreCase) && 
+                EncodedBody.SequenceEqual(other.EncodedBody);
+        }
+
+    }
+}
---
+++ src/DataCore.Adapter.Json/ExtensionObjectConverter.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Text.Json;
+
+using DataCore.Adapter.Common;
+
+namespace DataCore.Adapter.Json {
+
+    /// <summary>
+    /// JSON converter for <see cref="ExtensionObject"/>.
+    /// </summary>
+    public class ExtensionObjectConverter : AdapterJsonConverter<ExtensionObject> {
+
+
+        /// <inheritdoc/>
+        public override ExtensionObject Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) {
+            if (reader.TokenType != JsonTokenType.StartObject) {
+                ThrowInvalidJsonError();
+            }
+
+            Uri typeId = null!;
+            string encoding = null!;
+            byte[] encodedBody = null!;
+
+            while (reader.Read() && reader.TokenType != JsonTokenType.EndObject) {
+                if (reader.TokenType != JsonTokenType.PropertyName) {
+                    continue;
+                }
+
+                var propertyName = reader.GetString();
+                if (!reader.Read()) {
+                    ThrowInvalidJsonError();
+                }
+
+                if (string.Equals(propertyName, nameof(ExtensionObject.TypeId), StringComparison.OrdinalIgnoreCase)) {
+                    typeId = JsonSerializer.Deserialize<Uri>(ref reader, options)!;
+                }
+                else if (string.Equals(propertyName, nameof(ExtensionObject.Encoding), StringComparison.OrdinalIgnoreCase)) {
+                    encoding = JsonSerializer.Deserialize<string>(ref reader, options)!;
+                }
+                else if (string.Equals(propertyName, nameof(ExtensionObject.EncodedBody), StringComparison.OrdinalIgnoreCase)) {
+                    // Body is encoded as a base64web string.
+                    var base64webBody = JsonSerializer.Deserialize<string>(ref reader, options);
+                    if (base64webBody == null) {
+                        encodedBody = Array.Empty<byte>();
+                    }
+                    else {
+                        encodedBody = Base64WebDecode(base64webBody);
+                    }
+                }
+                else {
+                    reader.Skip();
+                }
+            }
+
+            return new ExtensionObject(typeId, encoding, encodedBody);
+        }
+
+
+        /// <inheritdoc/>
+        public override void Write(Utf8JsonWriter writer, ExtensionObject value, JsonSerializerOptions options) {
+            if (value == null) {
+                writer.WriteNullValue();
+                return;
+            }
+
+            writer.WriteStartObject();
+            WritePropertyValue(writer, nameof(ExtensionObject.TypeId), value.TypeId, options);
+            WritePropertyValue(writer, nameof(ExtensionObject.Encoding), value.Encoding, options);
+            WritePropertyValue(writer, nameof(ExtensionObject.EncodedBody), Base64WebEncode(value.EncodedBody), options);
+            writer.WriteEndObject();
+        }
+
+
+        /// <summary>
+        /// Converts the specified bytes to a base64web string.
+        /// </summary>
+        /// <param name="bytes">
+        ///   The bytes.
+        /// </param>
+        /// <returns>
+        ///   The encoded base64web string.
+        /// </returns>
+        private static string Base64WebEncode(byte[] bytes) {
+            return Convert.ToBase64String(bytes).Replace('/', '_').Replace('+', '-').TrimEnd('=');
+        }
+
+
+        /// <summary>
+        /// Converts the specified base64web string to a byte array.
+        /// </summary>
+        /// <param name="bytes">
+        ///   The base64web-encoded byte string.
+        /// </param>
+        /// <returns>
+        ///   The byte array.
+        /// </returns>
+        private static byte[] Base64WebDecode(string bytes) {
+            var base64 = bytes
+                .Replace('-', '+')
+                .Replace('_', '/');
+
+            switch (base64.Length % 4) {
+                case 0:
+                    // No padding required
+                    break;
+                case 2:
+                    // Pad with '=='
+                    base64 = base64 + "==";
+                    break;
+                case 3:
+                    // Pad with '='
+                    base64 = base64 + "=";
+                    break;
+            }
+
+            return Convert.FromBase64String(base64);
+        }
+
+
+        /// <summary>
+        /// Creates a new <see cref="ExtensionObject"/> with the specified type ID and value that 
+        /// encodes the value using JSON.
+        /// </summary>
+        /// <typeparam name="T">
+        ///   The value type.
+        /// </typeparam>
+        /// <param name="typeId">
+        ///   The type ID.
+        /// </param>
+        /// <param name="value">
+        ///   The value.
+        /// </param>
+        /// <param name="options">
+        ///   Options to control the conversion behaviour.
+        /// </param>
+        /// <returns>
+        ///   A new <see cref="ExtensionObject"/> with an <see cref="ExtensionObject.Encoding"/> 
+        ///   set to <c>application/json</c> and a JSON-encoded <see cref="ExtensionObject.EncodedBody"/>.
+        /// </returns>
+        public static ExtensionObject CreateExtensionObject<T>(Uri typeId, T value, JsonSerializerOptions? options = null) {
+            if (typeId == null) {
+                throw new ArgumentNullException(nameof(typeId));
+            }
+
+            if (value == null) {
+                return new ExtensionObject(typeId, "application/json", Array.Empty<byte>());
+            }
+
+            return new ExtensionObject(typeId, "application/json", JsonSerializer.SerializeToUtf8Bytes(value, options));
+        }
+
+    }
+}
