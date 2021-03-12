e347e5e6f6e307088beee6618a8a86d6139c3035
Graham Watts <grahamcharleswatts@gmail.com>
2021-03-12 17:06:19 +0200

    Add Newtonsoft.Json variant converter

---
+++ src/DataCore.Adapter.Json.Newtonsoft/JsonSerializerSettingsExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Newtonsoft.Json;
+
+namespace DataCore.Adapter.NewtonsoftJson {
+
+    /// <summary>
+    /// Extensions for <see cref="JsonSerializerSettings"/>.
+    /// </summary>
+    public static class JsonSerializerSettingsExtensions {
+
+        /// <summary>
+        /// Registers adapter-related converters.
+        /// </summary>
+        /// <param name="settings">
+        ///   The JSON serialization settings.
+        /// </param>
+        public static void AddDataCoreAdapterConverters(this JsonSerializerSettings settings) {
+            if (settings == null) {
+                throw new ArgumentNullException(nameof(settings));
+            }
+
+            settings.Converters.AddDataCoreAdapterConverters();
+
+        }
+
+
+        /// <summary>
+        /// Registers adapter-related converters.
+        /// </summary>
+        /// <param name="converters">
+        ///   The JSON converter collection to add new converters to.
+        /// </param>
+        public static void AddDataCoreAdapterConverters(this ICollection<JsonConverter> converters) {
+            if (converters == null) {
+                throw new ArgumentNullException(nameof(converters));
+            }
+
+            converters.Add(new VariantConverter());
+
+        }
+
+    }
+}
