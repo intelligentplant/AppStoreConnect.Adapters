baeb581e709e72d8c15db93865da57bd764812b8
Graham Watts <grahamcharleswatts@gmail.com>
2021-06-28 15:06:10 +0300

    Add Microsoft FASTER implementation and tests

---
+++ src/DataCore.Adapter.KeyValueStore.FASTER/README.md
@@ -0,0 +1,5 @@
+﻿# DataCore.Adapter.KeyValueStore.FASTER
+
+Implementation of [IKeyValueStore](/src/DataCore.Adapter.Abstractions/Services/IKeyValueStore.cs) that uses [Microsoft FASTER](https://microsoft.github.io/FASTER/) to persist data.
+
+The [FasterKeyValueStore](./FasterKeyValueStore.cs) implementation is based on code and concepts from [Jering.KeyValueStore](https://github.com/JeringTech/KeyValueStore). See [here](/THIRD_PARTY_LICENSES) for licence information.
---
+++ test/DataCore.Adapter.Tests/FasterKeyValueStoreTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DataCore.Adapter.KeyValueStore.FASTER;
+using DataCore.Adapter.Services;
+
+using FASTER.core;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataCore.Adapter.Tests {
+
+    [TestClass]
+    public class FasterKeyValueStoreTests : KeyValueStoreTests<FasterKeyValueStore> {
+        protected override FasterKeyValueStore CreateStore() {
+            return new FasterKeyValueStore(new FasterKeyValueStoreOptions());
+        }
+
+
+        [TestMethod]
+        public async Task ShouldPersistStateBetweenRestarts() {
+            var tmpPath = new DirectoryInfo(Path.Combine(Path.GetTempPath(), nameof(FasterKeyValueStoreTests), Guid.NewGuid().ToString()));
+
+            try {
+                var now = DateTime.UtcNow;
+
+                using (var store1 = new FasterKeyValueStore(new FasterKeyValueStoreOptions() { 
+                    CheckpointManagerFactory = () => FasterKeyValueStore.CreateLocalStorageCheckpointManager(tmpPath.FullName)
+                })) {
+
+                    var writeResult = await store1.WriteAsync(TestContext.TestName, now);
+                    Assert.AreEqual(KeyValueStoreOperationStatus.OK, writeResult);
+
+                    // Checkpoint should be created when we dispose because we have specified a
+                    // checkpoint manager.
+                }
+
+                using (var store2 = new FasterKeyValueStore(new FasterKeyValueStoreOptions() {
+                    CheckpointManagerFactory = () => FasterKeyValueStore.CreateLocalStorageCheckpointManager(tmpPath.FullName)
+                })) {
+                    var readResult = await store2.ReadAsync<DateTime>(TestContext.TestName);
+                    Assert.AreEqual(KeyValueStoreOperationStatus.OK, readResult.Status);
+                    Assert.AreEqual(now, readResult.Value);
+                }
+            }
+            finally {
+                tmpPath.Delete(true);
+            }
+        }
+    }
+
+}
