b03351555658e3fd6418be644d5cc96238ca7b14
Graham Watts <grahamcharleswatts@gmail.com>
2021-09-01 15:25:54 +0300

    Add JSON schema functionality

---
+++ src/DataCore.Adapter/Json/Schema/TimeSpanSchemaGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using Json.Schema;
+using Json.Schema.Generation;
+using Json.Schema.Generation.Generators;
+using Json.Schema.Generation.Intents;
+
+namespace DataCore.Adapter.Json.Schema {
+
+    /// <summary>
+    /// <see cref="ISchemaGenerator"/> for <see cref="TimeSpan"/>.
+    /// </summary>
+    internal class TimeSpanSchemaGenerator : ISchemaGenerator {
+
+        /// <inheritdoc/>
+        public bool Handles(Type type) {
+            return type == typeof(TimeSpan) || type == typeof(TimeSpan?);
+        }
+
+
+        /// <inheritdoc/>
+        public void AddConstraints(SchemaGeneratorContext context) {
+            context.Intents.Add(new TypeIntent(SchemaValueType.String));
+            context.Intents.Add(new TimeSpanFormatIntent());
+        }
+
+    }
+}
