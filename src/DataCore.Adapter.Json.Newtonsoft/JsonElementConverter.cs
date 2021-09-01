c6882fbc2845a559ac8a8730c6433392aaf3caa3
Graham Watts <grahamcharleswatts@gmail.com>
2021-09-01 15:30:37 +0300

    Update Newtonsoft JSON converters

---
+++ src/DataCore.Adapter.Json.Newtonsoft/JsonElementConverter.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using System.Text.Json;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace DataCore.Adapter.NewtonsoftJson {
+
+    /// <summary>
+    /// <see cref="JsonConverter{T}"/> for <see cref="JsonElement"/> from the <c>System.Text.Json</c> 
+    /// serializer.
+    /// </summary>
+    public class JsonElementConverter : JsonConverter<JsonElement> { 
+
+        /// <inheritdoc/>
+        public override void WriteJson(JsonWriter writer, JsonElement value, Newtonsoft.Json.JsonSerializer serializer) {
+            var json = System.Text.Json.JsonSerializer.Serialize(value);
+            writer.WriteRawValue(json);
+        }
+
+
+        /// <inheritdoc/>
+        public override JsonElement ReadJson(JsonReader reader, Type objectType, JsonElement existingValue, bool hasExistingValue, Newtonsoft.Json.JsonSerializer serializer) {
+            var token = serializer.Deserialize<JToken>(reader);
+            if (token == null) {
+                return default;
+            }
+
+            return JsonDocument.Parse(token.ToString(serializer.Formatting, serializer.Converters.ToArray())).RootElement;
+        }
+
+    }
+
+
+    /// <summary>
+    /// <see cref="JsonConverter{T}"/> for a nullable <see cref="JsonElement"/>.
+    /// </summary>
+    public class NullableJsonElementConverter : JsonConverter<JsonElement?> {
+
+        /// <inheritdoc/>
+        public override void WriteJson(JsonWriter writer, JsonElement? value, Newtonsoft.Json.JsonSerializer serializer) {
+            if (value == null) {
+                writer.WriteNull();
+                return;
+            }
+            var json = System.Text.Json.JsonSerializer.Serialize(value.Value);
+            writer.WriteRawValue(json);
+        }
+
+
+        /// <inheritdoc/>
+        public override JsonElement? ReadJson(JsonReader reader, Type objectType, JsonElement? existingValue, bool hasExistingValue, Newtonsoft.Json.JsonSerializer serializer) {
+            var token = serializer.Deserialize<JToken>(reader);
+            if (token == null) {
+                return default;
+            }
+
+            return JsonDocument.Parse(token.ToString(serializer.Formatting, serializer.Converters.ToArray())).RootElement;
+        }
+
+    }
+
+}
