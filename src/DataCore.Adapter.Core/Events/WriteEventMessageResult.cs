29962ab65c92e353d5580df0f879373392345108
Graham Watts <grahamcharleswatts@gmail.com>
2021-09-07 16:50:30 +0300

    Clean up namespace imports

--- src/DataCore.Adapter.Core/Events/WriteEventMessageResult.cs
+++ src/DataCore.Adapter.Core/Events/WriteEventMessageResult.cs
@@ -1,6 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
+
 using DataCore.Adapter.Common;
 
 namespace DataCore.Adapter.Events {
--- src/DataCore.Adapter.Core/RealTimeData/WriteTagValueAnnotationResult.cs
+++ src/DataCore.Adapter.Core/RealTimeData/WriteTagValueAnnotationResult.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
+
 using DataCore.Adapter.Common;
 
 namespace DataCore.Adapter.RealTimeData {
