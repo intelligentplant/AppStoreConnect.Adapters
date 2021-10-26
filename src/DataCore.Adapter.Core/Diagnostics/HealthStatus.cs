6498042da8bb1cbf3f452c4698599cf450fe0d33
Graham Watts <grahamcharleswatts@gmail.com>
2021-10-26 17:51:21 +0300

    Re-add HealthStatus compatibility
    Re-add obsolete HealthStatus enum and update HealthCheckResultConverter so that it can handle legacy health check results.

---
+++ src/DataCore.Adapter.Core/Diagnostics/HealthStatus.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DataCore.Adapter.Diagnostics {
+
+    /// <summary>
+    /// Represents the reported status of a health check result.
+    /// </summary>
+    [Obsolete("HealthStatus is deprecated in favour of StatusCode", false)]
+    public enum HealthStatus {
+
+        /// <summary>
+        /// Indicates that the health check determined that the component was unhealthy, or an 
+        /// unhandled exception was thrown while executing the health check.
+        /// </summary>
+        Unhealthy = 0,
+
+        /// <summary>
+        /// Indicates that the health check determined that the component was in a degraded state.
+        /// </summary>
+        Degraded = 1,
+
+        /// <summary>
+        /// Indicates that the health check determined that the component was healthy.
+        /// </summary>
+        Healthy = 2
+
+    }
+}
--- src/DataCore.Adapter.Json/HealthCheckResultConverter.cs
+++ src/DataCore.Adapter.Json/HealthCheckResultConverter.cs
@@ -19,7 +19,7 @@ namespace DataCore.Adapter.Json {
             }
 
             string? displayName = default;
-            StatusCode status = default;
+            StatusCode? status = null;
             string? description = default;
             string? error = default;
             IDictionary<string, string>? data = default;
@@ -41,6 +41,25 @@ namespace DataCore.Adapter.Json {
                 else if (string.Equals(propertyName, nameof(HealthCheckResult.StatusCode), StringComparison.OrdinalIgnoreCase)) {
                     status = JsonSerializer.Deserialize<StatusCode>(ref reader, options);
                 }
+                else if (string.Equals(propertyName, "Status", StringComparison.OrdinalIgnoreCase)) {
+                    // Backwards compatibility for older HealthCheckResult definition.
+                    if (!status.HasValue) {
+#pragma warning disable CS0618 // Type or member is obsolete
+                        var valueStatus = JsonSerializer.Deserialize<HealthStatus>(ref reader, options);
+                        switch (valueStatus) {
+                            case HealthStatus.Healthy:
+                                status = StatusCodes.Good;
+                                break;
+                            case HealthStatus.Unhealthy:
+                                status = StatusCodes.Bad;
+                                break;
+                            case HealthStatus.Degraded:
+                                status = StatusCodes.Uncertain;
+                                break;
+                        }
+#pragma warning restore CS0618 // Type or member is obsolete
+                    }
+                }
                 else if (string.Equals(propertyName, nameof(HealthCheckResult.Description), StringComparison.OrdinalIgnoreCase)) {
                     description = JsonSerializer.Deserialize<string>(ref reader, options);
                 }
@@ -58,7 +77,7 @@ namespace DataCore.Adapter.Json {
                 }
             }
 
-            return new HealthCheckResult(displayName, status, description, error, data, innerResults);
+            return new HealthCheckResult(displayName, status ?? StatusCodes.Uncertain, description, error, data, innerResults);
         }
 
         /// <inheritdoc/>
