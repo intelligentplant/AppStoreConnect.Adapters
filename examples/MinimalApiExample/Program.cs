a2f1c893ff4bce3c8a81d95708fe8ad13647f0df
Graham Watts <grahamcharleswatts@gmail.com>
2022-02-12 10:20:03 +0200

    Add minimal API example
    - Add a .NET 6 minimal API example adapter host
    - Add constructor overload for WaveGeneratorAdapter that accepts IOptions<TAdapterOptions>

---
+++ examples/MinimalApiExample/Program.cs
@@ -0,0 +1,58 @@
+﻿using DataCore.Adapter.WaveGenerator;
+
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+[assembly: DataCore.Adapter.VendorInfo("Intelligent Plant", "https://appstore.intelligentplant.com")]
+
+const string AdapterId = "$default";
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddDataCoreAdapterAspNetCoreServices()
+    .AddHostInfo(
+        name: "ASP.NET Core Minimal API Example",
+        description: "Example ASP.NET Core adapter host using minimal API syntax"
+     )
+    .AddServices(svc => svc.Configure<WaveGeneratorAdapterOptions>(
+        builder.Configuration.GetSection($"AppStoreConnect:Adapter:Settings")
+     ))
+    .AddAdapter(sp => ActivatorUtilities.CreateInstance<WaveGeneratorAdapter>(sp, AdapterId));
+
+builder.Services
+    .AddMvc()
+    .AddJsonOptions(options => options.JsonSerializerOptions.WriteIndented = true)
+    .AddDataCoreAdapterMvc();
+
+builder.Services
+    .AddSignalR()
+    .AddDataCoreAdapterSignalR();
+
+builder.Services
+    .AddGrpc();
+
+builder.Services
+    .AddHealthChecks()
+    .AddAdapterHealthChecks();
+
+builder.Services.AddOpenTelemetryTracing(otel => otel
+    .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("Adapter API"))
+    .AddAspNetCoreInstrumentation()
+    .AddDataCoreAdapterInstrumentation()
+    .AddJaegerExporter()
+    .AddConsoleExporter());
+
+var app = builder.Build();
+
+app.MapControllers();
+app.MapDataCoreAdapterHubs();
+app.MapDataCoreGrpcServices();
+app.MapHealthChecks("/health");
+
+app.MapFallback("/{*url}", context => {
+    context.Response.Redirect($"/api/app-store-connect/v2.0/host-info/");
+    return Task.CompletedTask;
+});
+
+app.Run();
