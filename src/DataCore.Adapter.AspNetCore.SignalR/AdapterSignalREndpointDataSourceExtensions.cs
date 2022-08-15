10a81c9ab74d772025a0453e6aaf74ff5e7fe046
Graham Watts <grahamcharleswatts@gmail.com>
2022-08-15 09:50:28 +0300

    API endpoint registration check extensions
    Adds extensions for `EndpointDataSource` for checking if the adapter MVC, gRPC and SignalR routes have been added to the request pipeline.

---
+++ src/DataCore.Adapter.AspNetCore.SignalR/AdapterSignalREndpointDataSourceExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+
+namespace Microsoft.AspNetCore.Routing {
+
+    /// <summary>
+    /// Extensions for <see cref="EndpointDataSource"/>.
+    /// </summary>
+    public static class AdapterSignalREndpointDataSourceExtensions {
+
+        /// <summary>
+        /// Checks if the App Store Connect Adapter SignalR API routes are registered with the 
+        /// <see cref="EndpointDataSource"/>.
+        /// </summary>
+        /// <param name="endpointDataSource">
+        ///   The <see cref="EndpointDataSource"/>.
+        /// </param>
+        /// <returns>
+        ///   <see langword="true"/> if the App Store Connect Adapter SignalR routes are registered, 
+        ///   or <see langword="false"/> otherwise.
+        /// </returns>
+        public static bool IsSignalRAdapterApiRegistered(this EndpointDataSource endpointDataSource) {
+            return endpointDataSource.Endpoints.Any(x => x is RouteEndpoint endpoint && endpoint.RoutePattern.RawText != null && endpoint.RoutePattern.RawText.StartsWith("/signalr/app-store-connect/") && endpoint.Metadata.OfType<SignalR.HubMetadata>().Any());
+        }
+
+    }
+}
