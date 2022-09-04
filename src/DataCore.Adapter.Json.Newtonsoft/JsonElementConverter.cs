8a8a491661d8ff71ed93354f6d97ff889088aeb0
Graham Watts <grahamcharleswatts@gmail.com>
2022-09-04 12:11:29 +0300

    Create JsonElementConverter.cs

---
+++ src/DataCore.Adapter.Json.Newtonsoft/JsonElementConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace DataCore.Adapter.NewtonsoftJson {
+
+    /// <summary>
+    /// <see cref="JsonConverter{T}"/> for <see cref="System.Text.Json.JsonElement"/>.
+    /// </summary>
+    public class JsonElementConverter : JsonConverter<System.Text.Json.JsonElement> {
+
+        /// <inheritdoc/>
+        public override void WriteJson(JsonWriter writer, System.Text.Json.JsonElement value, JsonSerializer serializer) {
+            writer.WriteRawValue(value.ToString());
+        }
+
+
+        /// <inheritdoc/>
+        public override System.Text.Json.JsonElement ReadJson(JsonReader reader, Type objectType, System.Text.Json.JsonElement existingValue, bool hasExistingValue, JsonSerializer serializer) {
+            var token = serializer.Deserialize<JToken>(reader);
+            return token == null 
+                ? default 
+                : System.Text.Json.JsonSerializer.Deserialize<System.Text.Json.JsonElement>(token.ToString());
+        }
+
+    }
+}
