94078192e93339dd2023d637fa5e940fdc1c5e3c
Graham Watts <grahamcharleswatts@gmail.com>
2022-09-15 16:36:05 +0300

    Add netfx EventMessagePushWithTopics impl
    Adds NetFX implementation of EventMessagePushWithTopics for gRPC proxy using Grpc.Net.Client

--- src/DataCore.Adapter.Grpc.Proxy/Events/EventMessagePushWithTopicsImpl.cs
+++ src/DataCore.Adapter.Grpc.Proxy/Events/EventMessagePushWithTopicsImpl.cs
@@ -14,7 +14,7 @@ namespace DataCore.Adapter.Grpc.Proxy.Events {
     /// <summary>
     /// <see cref="IEventMessagePushWithTopics"/> implementation.
     /// </summary>
-    internal class EventMessagePushWithTopicsImpl : ProxyAdapterFeature, IEventMessagePushWithTopics {
+    internal partial class EventMessagePushWithTopicsImpl : ProxyAdapterFeature, IEventMessagePushWithTopics {
 
         /// <summary>
         /// Creates a new <see cref="EventMessagePushWithTopicsImpl"/> instance.
@@ -23,74 +23,6 @@ namespace DataCore.Adapter.Grpc.Proxy.Events {
         ///   The proxy that owns the instance.
         /// </param>
         public EventMessagePushWithTopicsImpl(GrpcAdapterProxy proxy) : base(proxy) { }
-
-
-        /// <inheritdoc/>
-        public async IAsyncEnumerable<Adapter.Events.EventMessage> Subscribe(
-            IAdapterCallContext context,
-            CreateEventMessageTopicSubscriptionRequest request,
-            IAsyncEnumerable<EventMessageSubscriptionUpdate> channel,
-            [EnumeratorCancellation]
-            CancellationToken cancellationToken
-        ) {
-            Proxy.ValidateInvocation(context, request, channel);
-
-            var client = CreateClient<EventsService.EventsServiceClient>();
-
-            var createSubscriptionMessage = new CreateEventTopicPushChannelMessage() {
-                AdapterId = AdapterId,
-                SubscriptionType = request.SubscriptionType == EventMessageSubscriptionType.Active
-                    ? EventSubscriptionType.Active
-                    : EventSubscriptionType.Passive
-            };
-            createSubscriptionMessage.Topics.Add(request.Topics?.Where(x => x != null) ?? Array.Empty<string>());
-            if (request.Properties != null) {
-                foreach (var item in request.Properties) {
-                    createSubscriptionMessage.Properties.Add(item.Key, item.Value ?? string.Empty);
-                }
-            }
-
-            using (var ctSource = Proxy.CreateCancellationTokenSource(cancellationToken))
-            using (var grpcStream = client.CreateEventTopicPushChannel(GetCallOptions(context, ctSource.Token))) {
-
-                // Create the subscription.
-                await grpcStream.RequestStream.WriteAsync(new CreateEventTopicPushChannelRequest() {
-                    Create = createSubscriptionMessage
-                }).ConfigureAwait(false);
-
-                // Stream subscription changes.
-                Proxy.BackgroundTaskService.QueueBackgroundWorkItem(async ct => {
-                    await foreach (var update in channel.WithCancellation(ct).ConfigureAwait(false)) {
-                        if (update == null) {
-                            continue;
-                        }
-
-                        var msg = new UpdateEventTopicPushChannelMessage() {
-                            Action = update.Action == Common.SubscriptionUpdateAction.Subscribe
-                                ? SubscriptionUpdateAction.Subscribe
-                                : SubscriptionUpdateAction.Unsubscribe
-                        };
-                        msg.Topics.Add(update.Topics.Where(x => x != null));
-                        if (msg.Topics.Count == 0) {
-                            continue;
-                        }
-
-                        await grpcStream.RequestStream.WriteAsync(new CreateEventTopicPushChannelRequest() {
-                            Update = msg
-                        }).ConfigureAwait(false);
-                    }
-                }, ctSource.Token);
-
-                // Stream the results.
-                while (await grpcStream.ResponseStream.MoveNext(ctSource.Token).ConfigureAwait(false)) {
-                    if (grpcStream.ResponseStream.Current == null) {
-                        continue;
-                    }
-
-                    yield return grpcStream.ResponseStream.Current.ToAdapterEventMessage();
-                }
-            }
-        }
 
     }
 }
---
+++ src/DataCore.Adapter.Grpc.Proxy/Events/EventMessagePushWithTopicsImpl.netfx.cs
@@ -0,0 +1,162 @@
+﻿#if NETFRAMEWORK
+
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+using DataCore.Adapter.Events;
+
+using IntelligentPlant.BackgroundTasks;
+
+namespace DataCore.Adapter.Grpc.Proxy.Events {
+
+    partial class EventMessagePushWithTopicsImpl {
+
+        /// <summary>
+        /// All active streaming calls, indexed by subscription ID and then tag ID.
+        /// </summary>
+        private readonly ConcurrentDictionary<string, ConcurrentDictionary<string, IDisposable>> _activeStreamingCalls = new ConcurrentDictionary<string, ConcurrentDictionary<string, IDisposable>>(StringComparer.Ordinal);
+
+
+        /// <summary>
+        /// Creates a new <see cref="EventMessagePushWithTopics"/> instance that will handle all 
+        /// individual tag subscriptions for a single call to <see cref="Subscribe"/>.
+        /// </summary>
+        /// <param name="context">
+        ///   The <see cref="IAdapterCallContext"/> for the caller.
+        /// </param>
+        /// <param name="client">
+        ///   The gRPC client to use.
+        /// </param>
+        /// <param name="request">
+        ///   The initial <see cref="CreateEventMessageTopicSubscriptionRequest"/>.
+        /// </param>
+        /// <param name="cancellationToken">
+        ///   The cancellation token for the subscription.
+        /// </param>
+        /// <returns>
+        ///   A new <see cref="EventMessagePushWithTopics"/> instance.
+        /// </returns>
+        private EventMessagePushWithTopics CreateInnerHandler(
+            IAdapterCallContext context,
+            EventsService.EventsServiceClient client,
+            CreateEventMessageTopicSubscriptionRequest request,
+            CancellationToken cancellationToken
+        ) {
+            var options = new EventMessagePushWithTopicsOptions() {
+                OnTopicSubscriptionsAdded = async (instance, topics, _) => await OnTagsAddedAsync(instance, context, client, topics, request, cancellationToken).ConfigureAwait(false),
+                OnTopicSubscriptionsRemoved = async (instance, topics, _) => await OnTagsRemovedAsync(instance, topics).ConfigureAwait(false)
+            };
+            var result = new EventMessagePushWithTopics(options, BackgroundTaskService, Logger);
+
+            return result;
+        }
+
+
+        /// <summary>
+        /// Called when tags are added to the subscription.
+        /// </summary>
+        private Task OnTagsAddedAsync(
+            EventMessagePushWithTopics instance,
+            IAdapterCallContext context,
+            EventsService.EventsServiceClient client,
+            IEnumerable<string> topics,
+            CreateEventMessageTopicSubscriptionRequest request,
+            CancellationToken cancellationToken
+        ) {
+            foreach (var topic in topics) {
+                cancellationToken.ThrowIfCancellationRequested();
+
+                var grpcRequest = new CreateEventTopicPushChannelMessage() {
+                    AdapterId = Proxy.RemoteDescriptor.Id,
+                    SubscriptionType = request.SubscriptionType == EventMessageSubscriptionType.Active
+                        ? EventSubscriptionType.Active
+                        : EventSubscriptionType.Passive
+                };
+
+                grpcRequest.Topics.Add(topic);
+
+                if (request.Properties != null) {
+                    foreach (var item in request.Properties) {
+                        grpcRequest.Properties.Add(item.Key, item.Value ?? string.Empty);
+                    }
+                }
+
+                var subscription = client.CreateFixedEventTopicPushChannel(grpcRequest, GetCallOptions(context, cancellationToken));
+
+                var activeCallsForSubscriber = _activeStreamingCalls.GetOrAdd(instance.Id, _ => new ConcurrentDictionary<string, IDisposable>(StringComparer.OrdinalIgnoreCase));
+                activeCallsForSubscriber[topic] = subscription;
+
+                BackgroundTaskService.QueueBackgroundWorkItem(async ct => {
+                    while (await subscription.ResponseStream.MoveNext(ct).ConfigureAwait(false)) {
+                        if (subscription.ResponseStream.Current == null) {
+                            continue;
+                        }
+                        await instance.ValueReceived(subscription.ResponseStream.Current.ToAdapterEventMessage(), ct).ConfigureAwait(false);
+                    }
+                }, cancellationToken);
+            }
+
+            return Task.CompletedTask;
+        }
+
+
+        /// <summary>
+        /// Called when topics are removed from the subscription.
+        /// </summary>
+        private Task OnTagsRemovedAsync(
+            EventMessagePushWithTopics instance,
+            IEnumerable<string> topics
+        ) {
+            if (!_activeStreamingCalls.TryGetValue(instance.Id, out var activeCallsForSubscriber)) {
+                return Task.CompletedTask;
+            }
+
+            foreach (var topic in topics) {
+                if (activeCallsForSubscriber.TryRemove(topic, out var subscription)) {
+                    subscription.Dispose();
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+
+
+
+        /// <inheritdoc />
+        public async IAsyncEnumerable<Adapter.Events.EventMessage> Subscribe(
+            IAdapterCallContext context,
+            CreateEventMessageTopicSubscriptionRequest request,
+            IAsyncEnumerable<EventMessageSubscriptionUpdate> channel,
+            [EnumeratorCancellation]
+            CancellationToken cancellationToken
+        ) {
+            Proxy.ValidateInvocation(context, request, channel);
+
+            var client = CreateClient<EventsService.EventsServiceClient>();
+
+            using var ctSource = Proxy.CreateCancellationTokenSource(cancellationToken);
+            using var handler = CreateInnerHandler(context, client, request, ctSource.Token);
+
+            try {
+                await foreach (var item in handler.Subscribe(context, request, channel, ctSource.Token).ConfigureAwait(false)) {
+                    yield return item;
+                }
+            }
+            finally {
+                if (_activeStreamingCalls.TryRemove(handler.Id, out var activeCallsForSubscriber)) {
+                    foreach (var item in activeCallsForSubscriber.Values) {
+                        item.Dispose();
+                    }
+                }
+            }
+        }
+
+    }
+
+}
+
+#endif
