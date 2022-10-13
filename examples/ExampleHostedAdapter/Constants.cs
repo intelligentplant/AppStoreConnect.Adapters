d5c7cf6fcfda7e4dcef8416f201da11fe29a0193
Graham Watts <grahamcharleswatts@gmail.com>
2022-10-13 11:56:50 +0300

    Example adapter ID is now a GUID
    Also updates the template to generate a new adapter ID GUID in each new project

--- examples/ExampleHostedAdapter/Constants.cs
+++ examples/ExampleHostedAdapter/Constants.cs
@@ -8,7 +8,7 @@
         /// <summary>
         /// The ID of the hosted adapter.
         /// </summary>
-        public const string AdapterId = "$default";
+        public const string AdapterId = "e445a468-19ee-456c-9aac-e26288475a45";
 
         /// <summary>
         /// The path to the adapter settings JSON file.
