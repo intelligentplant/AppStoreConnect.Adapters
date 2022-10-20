0d0e9fa3a288b98fccb07235da592ab8aaf30350
Graham Watts <grahamcharleswatts@gmail.com>
2022-10-20 09:22:49 +0300

    Add request types

---
+++ src/DataCore.Adapter.Core/Tags/CreateTagRequest.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json;
+
+using DataCore.Adapter.Common;
+
+namespace DataCore.Adapter.Tags {
+
+    /// <summary>
+    /// A request to create a new tag definition.
+    /// </summary>
+    /// <seealso cref="GetTagSchemaRequest"/>
+    public class CreateTagRequest : AdapterRequest {
+
+        /// <summary>
+        /// The request body.
+        /// </summary>
+        /// <remarks>
+        ///   The schema for the <see cref="Body"/> is obtained by sending a <see cref="GetTagSchemaRequest"/> 
+        ///   to the adapter.
+        /// </remarks>
+        [Required]
+        public JsonElement Body { get; set; }
+
+    }
+}
