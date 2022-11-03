1447e0b1c8367a1f5aaa1991f50c7bb7c5444e92
Graham Watts <grahamcharleswatts@gmail.com>
2022-11-03 13:28:19 +0200

    Add PlotValueSelector

---
+++ src/DataCore.Adapter/RealTimeData/Utilities/PlotValue.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DataCore.Adapter.RealTimeData.Utilities {
+    public readonly struct PlotValue {
+
+        public readonly TagValueExtended Sample { get; }
+
+        public readonly string[]? Criteria { get; }
+
+
+        public PlotValue(TagValueExtended sample, IEnumerable<string>? criteria) {
+            Sample = sample;
+            Criteria = criteria?.ToArray();
+        }
+
+
+        public PlotValue(TagValueExtended sample, params string[] criteria) {
+            Sample = sample;
+            Criteria = criteria;
+        }
+
+    }
+}
---
+++ src/DataCore.Adapter/RealTimeData/Utilities/PlotValueSelector.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+using DataCore.Adapter.Tags;
+
+namespace DataCore.Adapter.RealTimeData.Utilities {
+
+    /// <summary>
+    /// Selects the significant values to return from a <see cref="TagValueBucket"/> for a plot 
+    /// (best-fit) query being processed by <see cref="PlotHelper"/>.
+    /// </summary>
+    /// <param name="tag">
+    ///   The tag that the plot values are being selected for.
+    /// </param>
+    /// <param name="bucket">
+    ///   The <see cref="TagValueBucket"/> to select the values from.
+    /// </param>
+    /// <returns>
+    ///   A collection of <see cref="PlotValue"/> instances representing the values selected from 
+    ///   the <paramref name="bucket"/>.
+    /// </returns>
+    /// <seealso cref="PlotHelper.DefaultPlotValueSelector"/>
+    public delegate IEnumerable<PlotValue> PlotValueSelector(TagSummary tag, TagValueBucket bucket);
+
+}
