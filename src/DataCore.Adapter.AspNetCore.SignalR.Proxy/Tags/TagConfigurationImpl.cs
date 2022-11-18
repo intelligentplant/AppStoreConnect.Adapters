28688262a562ee07558fb860f0fa5673fd3393ce
Graham Watts <grahamcharleswatts@gmail.com>
2022-11-18 17:05:19 +0200

    Tag configuration proxies (#265)

---
+++ src/DataCore.Adapter.AspNetCore.SignalR.Proxy/Tags/TagConfigurationImpl.cs
@@ -0,0 +1,51 @@
+﻿using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+using DataCore.Adapter.Tags;
+
+namespace DataCore.Adapter.AspNetCore.SignalR.Proxy.Tags {
+
+    /// <summary>
+    /// Implements <see cref="ITagConfiguration"/>
+    /// </summary>
+    internal class TagConfigurationImpl : ProxyAdapterFeature, ITagConfiguration {
+
+        /// <summary>
+        /// Creates a new <see cref="TagConfigurationImpl"/> object.
+        /// </summary>
+        /// <param name="proxy">
+        ///   The owning proxy.
+        /// </param>
+        public TagConfigurationImpl(SignalRAdapterProxy proxy) : base(proxy) { }
+
+
+        /// <inheritdoc/>
+        public async Task<JsonElement> GetTagSchemaAsync(IAdapterCallContext context, GetTagSchemaRequest request, CancellationToken cancellationToken) {
+            var client = GetClient();
+            return await client.TagSearch.GetTagSchemaAsync(AdapterId, request, cancellationToken).ConfigureAwait(false);
+        }
+
+
+        /// <inheritdoc/>
+        public async Task<TagDefinition> CreateTagAsync(IAdapterCallContext context, CreateTagRequest request, CancellationToken cancellationToken) {
+            var client = GetClient();
+            return await client.TagSearch.CreateTagAsync(AdapterId, request, cancellationToken).ConfigureAwait(false);
+        }
+
+
+        /// <inheritdoc/>
+        public async Task<TagDefinition> UpdateTagAsync(IAdapterCallContext context, UpdateTagRequest request, CancellationToken cancellationToken) {
+            var client = GetClient();
+            return await client.TagSearch.UpdateTagAsync(AdapterId, request, cancellationToken).ConfigureAwait(false);
+        }
+
+
+        /// <inheritdoc/>
+        public async Task<bool> DeleteTagAsync(IAdapterCallContext context, DeleteTagRequest request, CancellationToken cancellationToken) {
+            var client = GetClient();
+            return await client.TagSearch.DeleteTagAsync(AdapterId, request, cancellationToken).ConfigureAwait(false);
+        }
+
+    }
+}
--- src/DataCore.Adapter.Core/Tags/CreateTagRequest.cs
+++ src/DataCore.Adapter.Core/Tags/CreateTagRequest.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json;
 
 using DataCore.Adapter.Common;
@@ -20,6 +21,59 @@ namespace DataCore.Adapter.Tags {
         /// </remarks>
         [Required]
         public JsonElement Body { get; set; }
+
+
+        /// <summary>
+        /// Creates a new <see cref="CreateTagRequest"/> with the specified body.
+        /// </summary>
+        /// <typeparam name="TBody">
+        ///   The type of the request body.
+        /// </typeparam>
+        /// <param name="body">
+        ///   The request body.
+        /// </param>
+        /// <param name="options">
+        ///   The <see cref="JsonSerializerOptions"/> to use when serializing the <paramref name="body"/> 
+        ///   to a <see cref="JsonElement"/>.
+        /// </param>
+        /// <returns>
+        ///   A new <see cref="CreateTagRequest"/> object.
+        /// </returns>
+        public static CreateTagRequest Create<TBody>(TBody body, JsonSerializerOptions? options = null) {
+            return new CreateTagRequest() {
+                Body = JsonSerializer.SerializeToElement(body, options)
+            };
+        }
+
+
+        /// <summary>
+        /// Creates a new <see cref="CreateTagRequest"/> with the specified body.
+        /// </summary>
+        /// <typeparam name="TBody">
+        ///   The type of the request body.
+        /// </typeparam>
+        /// <param name="body">
+        ///   The request body.
+        /// </param>
+        /// <param name="jsonTypeInfo">
+        ///   The <see cref="System.Text.Json.Serialization.Metadata.JsonTypeInfo{T}"/> to use 
+        ///   when serializing the <paramref name="body"/> to a <see cref="JsonElement"/>.
+        /// </param>
+        /// <returns>
+        ///   A new <see cref="CreateTagRequest"/> object.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        ///   <paramref name="jsonTypeInfo"/> is <see langword="null"/>.
+        /// </exception>
+        public static CreateTagRequest Create<TBody>(TBody body, System.Text.Json.Serialization.Metadata.JsonTypeInfo<TBody> jsonTypeInfo) {
+            if (jsonTypeInfo == null) {
+                throw new ArgumentNullException(nameof(jsonTypeInfo));
+            }
+
+            return new CreateTagRequest() {
+                Body = JsonSerializer.SerializeToElement(body, jsonTypeInfo)
+            };
+        }
 
     }
 }
