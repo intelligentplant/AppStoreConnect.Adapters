316fff036d94009bc194eda745a27df1bc4cc84f
Graham Watts <grahamcharleswatts@gmail.com>
2023-01-04 16:18:51 +0200

    Update tutorial code

--- examples/SimpleMqttExample/MqttAdapter/MyAdapterOptions.cs
+++ examples/SimpleMqttExample/MqttAdapter/MyAdapterOptions.cs
@@ -2,6 +2,8 @@
 
 using DataCore.Adapter;
 
+#nullable disable warnings
+
 namespace MqttAdapter {
 
     public class MyAdapterOptions : AdapterOptions {
@@ -9,14 +11,15 @@ namespace MqttAdapter {
         [Display(Description = "The MQTT server hostname")]
         [Required(ErrorMessage = "You must specify a hostname")]
         [MaxLength(500, ErrorMessage = "Hostname cannot be longer than 500 characters")]
-        public string Hostname { get; set; } = "test.mosquitto.org";
+        public string Hostname { get; set; }
 
         [Display(Description = "The port number for the MQTT server")]
         [Range(1, 65535, ErrorMessage = "Port must be in the range 1 - 65535")]
-        public int Port { get; set; } = 1883;
+        public int Port { get; set; }
 
         [Display(Description = "A comma-delimited list of MQTT topics to subscribe to")]
-        public string Topics { get; set; } = default!;
+        [Required(ErrorMessage = "You must specify a list of MQTT topics to subscribe to")]
+        public string Topics { get; set; }
 
     }
 
