f79a44a940e8522589b8db8f8a757bc4ab570da6
Graham Watts <grahamcharleswatts@gmail.com>
2023-02-22 09:18:05 +0200

    Add IAdapterCallContext<T>
    IAdapterCallContext<T> extends IAdapterCallContext to expose a provider that is used to implement the other IAdapterCallContext members.

---
+++ src/DataCore.Adapter.Abstractions/IAdapterCallContextT.cs
@@ -0,0 +1,20 @@
+﻿namespace DataCore.Adapter {
+
+    /// <summary>
+    /// Extends <see cref="IAdapterCallContext"/> to define a <see cref="Provider"/> that is used 
+    /// to implement the other <see cref="IAdapterCallContext"/> members.
+    /// </summary>
+    /// <typeparam name="T">
+    ///   The type of the provider.
+    /// </typeparam>
+    public interface IAdapterCallContext<T> : IAdapterCallContext {
+
+        /// <summary>
+        /// The provider that is used to implement the other <see cref="IAdapterCallContext"/> 
+        /// members.
+        /// </summary>
+        T Provider { get; }
+
+    }
+
+}
