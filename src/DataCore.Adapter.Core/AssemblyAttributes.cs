544caa43d46651a8dd64749b63b70db17dcfecfe
Graham Watts <graham.watts@intelligentplant.com>
2023-04-24 12:29:36 +0300

    Builders for adapter/adapter type descriptors (#326)
    * Use ArgumentOutOfRangeException in AdapterDescriptor
    
    Null/white space string arguments use ArgumentOutOfRangeException instead of ArgumentException, as the former is more specific.
    
    This is not considered a breaking change, as ArgumentOutOfRangeException is derived from ArgumentException.
    
    * Make internals in core project visible to abstractions project
    
    * Add adapter and type descriptor builders
    
    * Update extension methods to use builder types
    
    Use AdapterDescriptorBuilder and AdapterTypeDescriptorBuilder where appropriate.

--- src/DataCore.Adapter.Core/AssemblyAttributes.cs
+++ src/DataCore.Adapter.Core/AssemblyAttributes.cs
@@ -1,5 +1,9 @@
 ﻿using System.Runtime.CompilerServices;
 
+// DataCore.Adapter.Abstractions is allowed to use internals from this assembly for use in builder
+// types etc.
+[assembly: InternalsVisibleTo("DataCore.Adapter.Abstractions")]
+
 // DataCore.Adapter.Json is allowed to use internals from this assembly so that it can correctly 
 // deserialize Variant instances.
 [assembly: InternalsVisibleTo("DataCore.Adapter.Json")]
