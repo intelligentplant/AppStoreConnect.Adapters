23ec9fe89c6a6d6a0e376e9b3d8892b9c6e83f45
Graham Watts <grahamcharleswatts@gmail.com>
2024-04-15 09:39:57 +0300

    Tag value builder properties are now distinct
    `TagValueBuilder` now enforces that only one property with a given name can be created on a tag value. If property with the same name already exists when a property is set on the builder, the existing property is replaced.
    
    Various property-related methods have been moved to `TagValueBuilderExtensions` as extension methods. A new extension method for setting the value to a digital state has also been added; this will set both a numerical value and a display value.

---
+++ src/DataCore.Adapter/RealTimeData/TagValueBuilderExtensions.cs
@@ -0,0 +1,169 @@
+﻿using System;
+
+using DataCore.Adapter.Common;
+using DataCore.Adapter.RealTimeData.Utilities;
+
+namespace DataCore.Adapter.RealTimeData {
+
+    /// <summary>
+    /// Extension methods for <see cref="TagValueBuilder"/>.
+    /// </summary>
+    public static class TagValueBuilderExtensions {
+
+        /// <summary>
+        /// Adds a property to the tag value.
+        /// </summary>
+        /// <param name="builder">
+        ///   The <see cref="TagValueBuilder"/>.
+        /// </param>
+        /// <param name="name">
+        ///   The property name.
+        /// </param>
+        /// <param name="value">
+        ///   The property value.
+        /// </param>
+        /// <returns>
+        ///   The updated <see cref="TagValueBuilder"/>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        ///   <paramref name="builder"/> is <see langword="null"/>.
+        /// </exception>
+        public static TagValueBuilder WithProperty(this TagValueBuilder builder, string name, Variant value) {
+            if (builder == null) {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            if (name == null) {
+                return builder;
+            }
+
+            return builder.WithProperty(new AdapterProperty(name, value));
+        }
+
+
+        /// <summary>
+        /// Adds a property to the tag value.
+        /// </summary>
+        /// <typeparam name="T">
+        ///   The property value type.
+        /// </typeparam>
+        /// <param name="builder">
+        ///   The <see cref="TagValueBuilder"/>.
+        /// </param>
+        /// <param name="name">
+        ///   The property name.
+        /// </param>
+        /// <param name="value">
+        ///   The property value.
+        /// </param>
+        /// <returns>
+        ///   The updated <see cref="TagValueBuilder"/>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        ///   <paramref name="builder"/> is <see langword="null"/>.
+        /// </exception>
+        public static TagValueBuilder WithProperty<T>(this TagValueBuilder builder, string name, T value) {
+            if (builder == null) {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            if (name == null) {
+                return builder;
+            }
+
+            return builder.WithProperty(name, Variant.FromValue(value));
+        }
+
+
+        /// <summary>
+        /// Sets the value for the sample using a digital state definition.
+        /// </summary>
+        /// <param name="builder">
+        ///   The <see cref="TagValueBuilder"/>.
+        /// </param>
+        /// <param name="state">
+        ///   The digital state definition.
+        /// </param>
+        /// <returns>
+        ///   The <see cref="TagValueBuilder"/>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        ///   <paramref name="builder"/> is <see langword="null"/>.
+        /// </exception>
+        public static TagValueBuilder WithDigitalStateValue(this TagValueBuilder builder, Tags.DigitalState state) {
+            if (builder == null) {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            if (state == null) {
+                return builder;
+            }
+
+            return builder.WithValue(state.Value, state.Name)
+                .WithSteppedTransition(true);
+        }
+
+
+        /// <summary>
+        /// Specifies if a tag value represents a stepped value transition (such as a discrete 
+        /// value change on a digital tag or an analogue process limit). 
+        /// </summary>
+        /// <param name="builder">
+        ///   The <see cref="TagValueBuilder"/>.
+        /// </param>
+        /// <param name="stepped">
+        ///   <see langword="true"/> if the tag value represents a stepped value transition, or 
+        ///   <see langword="false"/> otherwise.
+        /// </param>
+        /// <returns>
+        ///   The updated <see cref="TagValueBuilder"/>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        ///   <paramref name="builder"/> is <see langword="null"/>.
+        /// </exception>
+        /// <remarks>
+        /// 
+        /// <para>
+        ///   Calling <see cref="WithSteppedTransition"/> sets the <see cref="WellKnownProperties.TagValue.Stepped"/> 
+        ///   property on the tag value.
+        /// </para>
+        /// 
+        /// <para>
+        ///   Consuming applications can use this property as a hint regarding how the time series 
+        ///   data should be visualized.
+        /// </para>
+        /// 
+        /// </remarks>
+        public static TagValueBuilder WithSteppedTransition(this TagValueBuilder builder, bool stepped) {
+            if (builder == null) {
+                throw new ArgumentNullException(nameof(builder));
+            }
+            return builder.WithProperty(string.Intern(WellKnownProperties.TagValue.Stepped), stepped);
+        }
+
+
+        /// <summary>
+        /// Adds a set of properties to the tag value being calculated from a bucket.
+        /// </summary>
+        /// <param name="builder">
+        ///   The <see cref="TagValueBuilder"/>.
+        /// </param>
+        /// <param name="bucket">
+        ///   The bucket.
+        /// </param>
+        /// <returns>
+        ///   The updated <see cref="TagValueBuilder"/>.
+        /// </returns>
+        internal static TagValueBuilder WithBucketProperties(this TagValueBuilder builder, TagValueBucket bucket) {
+            if (bucket != null) {
+                return builder.WithProperties(
+                    new AdapterProperty(string.Intern(CommonTagValuePropertyNames.BucketStart), bucket.UtcBucketStart),
+                    new AdapterProperty(string.Intern(CommonTagValuePropertyNames.BucketEnd), bucket.UtcBucketEnd)
+                );
+            }
+
+            return builder;
+        }
+
+    }
+}
