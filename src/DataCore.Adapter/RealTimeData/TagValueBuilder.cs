8b8df05bec433fd04218b0e05b5fcfa7864a0ae3
Graham Watts <grahamcharleswatts@gmail.com>
2024-04-17 08:50:46 +0300

    Namespace cleanup

--- src/DataCore.Adapter/RealTimeData/TagValueBuilder.cs
+++ src/DataCore.Adapter/RealTimeData/TagValueBuilder.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+
 using DataCore.Adapter.Common;
-using DataCore.Adapter.RealTimeData.Utilities;
 
 namespace DataCore.Adapter.RealTimeData {
 
