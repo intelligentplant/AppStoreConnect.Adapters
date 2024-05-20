d5dd8add81b37b26152620c9b69911267c6eac71
Graham Watts <grahamcharleswatts@gmail.com>
2024-05-20 10:00:03 +0300

    Enforce UTC conversion for Newtonsoft
    Modifies JsonSerializerSettingsExtensions to ensure that JsonSerializerSettings objects always have their DateTimeZoneHandling property set to DateTimeZoneHandling.Utc, and adds an IsoDateTimeConverter that always adjusts timestamps to UTC.

--- src/DataCore.Adapter.Json.Newtonsoft/JsonSerializerSettingsExtensions.cs
+++ src/DataCore.Adapter.Json.Newtonsoft/JsonSerializerSettingsExtensions.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 
 using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace DataCore.Adapter.NewtonsoftJson {
 
@@ -11,34 +12,43 @@ namespace DataCore.Adapter.NewtonsoftJson {
     public static class JsonSerializerSettingsExtensions {
 
         /// <summary>
-        /// Registers adapter-related converters.
+        /// Registers adapter-related converters and default serialization behaviours.
         /// </summary>
         /// <param name="settings">
         ///   The JSON serialization settings.
         /// </param>
-        public static void AddDataCoreAdapterConverters(this JsonSerializerSettings settings) {
+        public static void UseDataCoreAdapterDefaults(this JsonSerializerSettings settings) => settings.UseDataCoreAdapterDefaults(null);
+
+
+        /// <summary>
+        /// Registers adapter-related converters and default serialization behaviours.
+        /// </summary>
+        /// <param name="settings">
+        ///   The JSON serialization settings.
+        /// </param>
+        /// <param name="jsonElementConverterOptions">
+        ///   The <see cref="System.Text.Json.JsonSerializerOptions"/> to use with the <see cref="JsonElementConverter"/> 
+        ///   registered by this method.
+        /// </param>
+        public static void UseDataCoreAdapterDefaults(this JsonSerializerSettings settings, System.Text.Json.JsonSerializerOptions? jsonElementConverterOptions) {
             if (settings == null) {
                 throw new ArgumentNullException(nameof(settings));
             }
 
-            settings.AddDataCoreAdapterConverters(null);
-
+            // Ensure that DateTime values are always serialized as UTC
+            settings.DateTimeZoneHandling = DateTimeZoneHandling.Utc;
+            settings.Converters.AddDataCoreAdapterConverters(jsonElementConverterOptions);
         }
 
 
         /// <summary>
         /// Registers adapter-related converters.
         /// </summary>
-        /// <param name="converters">
-        ///   The JSON converter collection to add new converters to.
+        /// <param name="settings">
+        ///   The JSON serialization settings.
         /// </param>
-        public static void AddDataCoreAdapterConverters(this ICollection<JsonConverter> converters) {
-            if (converters == null) {
-                throw new ArgumentNullException(nameof(converters));
-            }
-
-            converters.AddDataCoreAdapterConverters(null);
-        }
+        [Obsolete("This method will be removed in a future version. Call UseDataCoreAdapterDefaults instead.", false)]
+        public static void AddDataCoreAdapterConverters(this JsonSerializerSettings settings) => settings.UseDataCoreAdapterDefaults();
 
 
         /// <summary>
@@ -51,14 +61,17 @@ namespace DataCore.Adapter.NewtonsoftJson {
         ///   The <see cref="System.Text.Json.JsonSerializerOptions"/> to use with the <see cref="JsonElementConverter"/> 
         ///   registered by this method.
         /// </param>
-        public static void AddDataCoreAdapterConverters(this JsonSerializerSettings settings, System.Text.Json.JsonSerializerOptions? jsonElementConverterOptions) {
-            if (settings == null) {
-                throw new ArgumentNullException(nameof(settings));
-            }
+        [Obsolete("This method will be removed in a future version. Call UseDataCoreAdapterDefaults instead.", false)]
+        public static void AddDataCoreAdapterConverters(this JsonSerializerSettings settings, System.Text.Json.JsonSerializerOptions? jsonElementConverterOptions) => settings.UseDataCoreAdapterDefaults(jsonElementConverterOptions);
 
-            settings.Converters.AddDataCoreAdapterConverters(jsonElementConverterOptions);
 
-        }
+        /// <summary>
+        /// Registers adapter-related converters.
+        /// </summary>
+        /// <param name="converters">
+        ///   The JSON converter collection to add new converters to.
+        /// </param>
+        public static void AddDataCoreAdapterConverters(this ICollection<JsonConverter> converters) => converters.AddDataCoreAdapterConverters(null);
 
 
         /// <summary>
@@ -80,6 +93,9 @@ namespace DataCore.Adapter.NewtonsoftJson {
             converters.Add(new NullableJsonElementConverter(jsonElementConverterOptions));
             converters.Add(new VariantConverter());
             converters.Add(new ByteStringConverter());
+            converters.Add(new IsoDateTimeConverter() { 
+                DateTimeStyles = System.Globalization.DateTimeStyles.AdjustToUniversal
+            });
         }
 
     }
