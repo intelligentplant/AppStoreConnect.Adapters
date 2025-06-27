85a7c481fcde4e6c59fecd22d50aa5e2580bc031
Graham Watts <grahamcharleswatts@gmail.com>
2025-06-27 11:29:17 +0300

    Update template source files
    Adds new template source files for adapter projects

---
+++ src/DataCore.Adapter.Templates/src/aschostedadapter/Example.Adapter.Tests/RngAdapterTests.cs
@@ -0,0 +1,30 @@
+﻿namespace Example.Adapter.Tests {
+
+    [TestClass]
+    public class RngAdapterTests : AdapterTestsBase<RngAdapter> {
+
+        private static IServiceProvider s_serviceProvider = null!;
+
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context) {
+            var services = new ServiceCollection();
+
+            services.AddLogging();
+
+            services.AddDataCoreAdapterServices()
+                .AddAdapterOptions<RngAdapterOptions>();
+
+            s_serviceProvider = services.BuildServiceProvider();
+        }
+
+
+        protected override IServiceScope? CreateServiceScope(TestContext context) => s_serviceProvider.CreateScope();
+
+
+        protected override RngAdapter CreateAdapter(TestContext context, IServiceProvider? serviceProvider) 
+            => ActivatorUtilities.CreateInstance<RngAdapter>(serviceProvider!, context.TestName!);
+
+    }
+
+}
